a42804be2644d321ea9a1c5cc7dece492a9a5929
felipe <pipe.castro.espinoza@gmail.com>
2021-11-06 10:28:52 -0600

    version 1.0

--- base/conexion.php
+++ base/conexion.php
@@ -4,7 +4,7 @@
     $user = "uso5ppkiztbkdnqn";
     $pass = "bfad9ultcsdfspqnv711";
     $db = "g2z3mvS5NdFaaPiEG7Rs";
-    $conexion = mysqli_connect($server,$user,$pass,$db);
+    $conexion = "mysql://uso5ppkiztbkdnqn:g2z3mvS5NdFaaPiEG7Rs@bfad9ultcsdfspqnv711-mysql.services.clever-cloud.com:3306/bfad9ultcsdfspqnv711";
     
     if($conexion){
         //echo "Conectado correctamente a la bd\n";
