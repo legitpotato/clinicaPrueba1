3398db3e480289a938693124771b811e0977690a
felipe <pipe.castro.espinoza@gmail.com>
2021-11-06 10:24:48 -0600

    Version 1.0

--- base/conexion.php
+++ base/conexion.php
@@ -1,15 +1,16 @@
 <?php
 
-    $server = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "clinica";
+    $server = "bfad9ultcsdfspqnv711-mysql.services.clever-cloud.com";
+    $user = "uso5ppkiztbkdnqn";
+    $pass = "bfad9ultcsdfspqnv711";
+    $db = "g2z3mvS5NdFaaPiEG7Rs";
     $conexion = mysqli_connect($server,$user,$pass,$db);
     
     if($conexion){
         //echo "Conectado correctamente a la bd\n";
     }
     else{
+        
         echo "No se pudo conectar";
     }
 
