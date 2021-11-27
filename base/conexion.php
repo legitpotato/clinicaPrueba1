e0cff193ae5986f60cb95bbe540255d49968bc30
Sebastián Saa <40745034+legitpotato@users.noreply.github.com>
2021-11-27 16:12:11 -0300

    Add files via upload

--- base/conexion.php
+++ base/conexion.php
@@ -1,16 +1,15 @@
 <?php
 
-    $server = "bfad9ultcsdfspqnv711-mysql.services.clever-cloud.com";
-    $user = "uso5ppkiztbkdnqn";
-    $pass = "bfad9ultcsdfspqnv711";
-    $db = "g2z3mvS5NdFaaPiEG7Rs";
-    $conexion = "mysql://uso5ppkiztbkdnqn:g2z3mvS5NdFaaPiEG7Rs@bfad9ultcsdfspqnv711-mysql.services.clever-cloud.com:3306/bfad9ultcsdfspqnv711";
+    $server = "bft4tkk8rm2x28aipdxo-mysql.services.clever-cloud.com";
+    $user = "uk6jrnxz2lrw4i6m";
+    $pass = "6WsyDL5ctp48vTUH6up9";
+    $db = "bft4tkk8rm2x28aipdxo";
+    $conexion = mysqli_connect($server,$user,$pass,$db);
     
     if($conexion){
         //echo "Conectado correctamente a la bd\n";
     }
     else{
-        
         echo "No se pudo conectar";
     }
 
