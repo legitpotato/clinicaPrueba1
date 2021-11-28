976a41680083ffc1c776cf7a0c49ee71b8537437
felipe <pipe.castro.espinoza@gmail.com>
2021-11-27 21:37:50 -0600

    genotipos

--- base/conexion.php
+++ base/conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-    $server = "bft4tkk8rm2x28aipdxo-mysql.services.clever-cloud.com";
-    $user = "uk6jrnxz2lrw4i6m";
-    $pass = "6WsyDL5ctp48vTUH6up9";
-    $db = "bft4tkk8rm2x28aipdxo";
+    $server = "bft4tkk8rm2x28aipdxo-mysql.services.clever-cloud.com"; //bft4tkk8rm2x28aipdxo-mysql.services.clever-cloud.com
+    $user = "uk6jrnxz2lrw4i6m";//uk6jrnxz2lrw4i6m
+    $pass = "6WsyDL5ctp48vTUH6up9";//6WsyDL5ctp48vTUH6up9
+    $db = "bft4tkk8rm2x28aipdxo";//bft4tkk8rm2x28aipdxo
     $conexion = mysqli_connect($server,$user,$pass,$db);
     
     if($conexion){
-        //echo "Conectado correctamente a la bd\n";
+        echo "Conectado correctamente a la base de datos\n";
     }
     else{
         echo "No se pudo conectar";
--- validacionDonante.php
+++ validacionDonante.php
@@ -17,8 +17,8 @@
     $Rh_k = $_POST["Rh-k"];
     $Rh_K = $_POST["Rh-K"];
 
-    $Rh_kpa = $_POST["Rh-kpa"];
-    $Rh_kpb = $_POST["Rh-kpb"];
+    $Rh_kpa = $_POST["Rh-Kpa"];
+    $Rh_kpb = $_POST["Rh-Kpb"];
 
     $Rh_Jsa = $_POST["Rh-Jsa"];
     $Rh_Jsb = $_POST["Rh-Jsb"]; 
@@ -30,15 +30,47 @@
     $Rh_Jkb = $_POST["Rh-Jkb"];
 
 
+    $query = "SELECT rut FROM `genotipos` WHERE c1 = '$Rh_c' ";
     
+    // -- c1 = '$Rh_c' AND 
+    // -- C  = '$Rh_C' AND
+    // -- e1 = '$Rh_e' AND
+    // -- E  = '$Rh_E' AND
+    // -- V  = '$Rh_V' AND
+    // -- VS = '$Rh_VS' AND
+    // -- K1 = '$Rh_k' AND
+    // -- k  = '$Rh_K' AND
+    // -- Kpa  = '$Rh_kpa' AND
+    // -- Kpb  = '$Rh_kpb' AND
+    // -- Jsa  = '$Rh_Jsa' AND
+    // -- Jsb  = '$Rh_Jsb' AND
+    // -- Fya  = '$Rh_Fya' AND
+    // -- Fyb  = '$Rh_Fyb' AND
+    // -- Jka  = '$Rh_Jka' AND
+    // -- Jkb  = '$Rh_Jkb' AND    
+    // -- ";
+
+ 
+    $resultado = mysqli_query($conexion,$query);
+    $filas = mysqli_num_rows($resultado);
+    echo $filas;
+
+    if($filas == false){
+        echo "puchi";
+    }
+
+    if($filas){
+        echo "Encontramos donante";
+    }
+    else{
+        echo "Error";
+    }
 
 
 
 
 
-    echo $Rh_c;
-    echo "\n";
-    echo $Rh_C;
 
+   
 
 ?>
